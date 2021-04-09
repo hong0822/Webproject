84707bdda944a1e46c9d6d966b5c39b91accc51d
choong jing hong <s19t15901@stu.edu.tw>
2021-04-09 16:46:26 +0800

    commit

--- README.md
+++ README.md
@@ -1,4 +1,5 @@
 # Webproject
 树德科技大学WEB APP课程练习专用
 
-
+# MY Idea
+介绍
---
+++ php/clothes.php
@@ -0,0 +1,39 @@
+<?php
+
+class Clothes {
+    private $name;
+    private $prize;
+
+    public function __construct($outsidename, $outsidePrize){
+        $this->name = $outsidename;
+        $this->prize = $outsidePrize;
+    }
+    
+    public function setName($outsidename){
+        $this->name = $outsidename;
+    }
+
+    public function getName(){
+        return $this->name;
+
+    }
+
+    public function setPrize($outsidePrize){
+        $this->prize = $outsidePrize;
+    }
+
+    public function getPrize(){
+        return $this->prize;
+
+    }
+
+    public function dowash(){
+        return "用洗衣机洗";
+    }
+
+    public function __destruct(){
+        
+    }
+}
+
+?>
